c9530a34bd7d1b6c7d360fbc10d0e8db763f62b8
Koppala Naveenkumar <naveenkumarkoppala@gmail.com>
2021-06-09 21:34:00 +0530

    update

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 // app.js
 const express = require('express');
+const dotenv = require('dotenv');
 // const bodyParser = require('body-parser');
 const app = express();
-
+dotenv.config();
 
 // Imports routes for the products
 const product = require('./routes/product.route');
 
 // Set uo mongoose connection
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://naveenkumar:naveenkumar@ecommerce.wxgng.mongodb.net/fake?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true}),(err)=>{
-    if(err){
-        console.log(err.message);
-    }
-    else{
-        console.log("Server Started")
-    }
-}
-
+const db = require('./utilities/db.config');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+db.connectToDB();
 // http://localhost:1234/products/test
 app.use('/products', product)
 // initialize our express app
 
 
-app.listen(1234, ()=>{
-    console.log("Server started at PORT: 1234")
+app.listen(process.env.PORT, ()=>{
+    console.log("Server started at PORT: " + `${process.env.PORT}`)
 })
